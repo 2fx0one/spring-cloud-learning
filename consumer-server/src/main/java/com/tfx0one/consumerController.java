b8d0b99e694c9930cc2bc111c522b8f7714911e5
2fx0one <2fx0one@gmail.com>
2019-03-01 00:02:12 +0800

    update

--- consumer-server/src/main/java/com/tfx0one/consumerController.java
+++ consumer-server/src/main/java/com/tfx0one/consumerController.java
@@ -5,7 +5,6 @@ package com.tfx0one;
  * 2019/2/28 21:41
  */
 
-import com.netflix.appinfo.InstanceInfo;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,7 +30,9 @@ public class consumerController {
         List<ServiceInstance> list = discoveryClient.getInstances("user-server");
 //        User u = restTemplate.getForObject("http://127.0.0.1:10001/sys/user/1", User.class);
         ServiceInstance instance = list.get(0);
+        System.out.println(instance);
         String baseUrl = "http://"+instance.getHost() + ":" + instance.getPort()+"/sys/user/1";
+        System.out.println(baseUrl);
         User u = restTemplate.getForObject(baseUrl, User.class);
         System.out.println(u);
         return u.getName();
