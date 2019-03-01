520da436043fb6648e2f0337d5ab84f5ceb2cfba
2fx0one <2fx0one@gmail.com>
2019-03-02 01:00:13 +0800

    update

--- consumer-server/src/main/java/com/tfx0one/ConsumerApplication.java
+++ consumer-server/src/main/java/com/tfx0one/ConsumerApplication.java
@@ -2,9 +2,19 @@ package com.tfx0one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /*
  * @Auth 2fx0one
@@ -12,13 +22,37 @@ import org.springframework.web.client.RestTemplate;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@RequestMapping("/consumer")
 public class ConsumerApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
     }
 
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+//        User u = restTemplate.getForObject("http://127.0.0.1:10001/sys/user/1", User.class);
+//        1、从注册中心获取实例
+//        List<ServiceInstance> list = discoveryClient.getInstances("user-server");
+//        ServiceInstance instance = list.get(0);
+//        System.out.println(instance);
+//        String baseUrl = "http://"+instance.getHost() + ":" + instance.getPort()+"/sys/user/1";
+//        2。 开启 Robbin负载均衡 loadBalanced
+        String baseUrl = "http://user-server" + "/sys/user/1";
+        System.out.println(baseUrl);
+        User u = restTemplate.getForObject(baseUrl, User.class);
+        System.out.println(u);
+        return u.getName();
+    }
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
